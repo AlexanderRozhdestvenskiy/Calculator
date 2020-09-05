18a75250dd9ba6d92bc365df95e0cdc2853e2a98
AlexanderRozhdestvenskiy <alexrozh1988@gmail.com>
2020-09-05 17:11:06 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Alexander Rozhdestvenskiy on 05.09.2020.
+//  Copyright © 2020 AlexRozh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
